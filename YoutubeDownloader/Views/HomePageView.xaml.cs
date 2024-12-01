a06f3db8b0d4e298b16bf9bc7eeee1ccdac41a4e
Ale <Ale@PC-ALE01>
2024-12-01 16:30:42 +0100

    UI: Avvio ricerca url con tasto Enter

--- YoutubeDownloader/Views/HomePageView.xaml.cs
+++ YoutubeDownloader/Views/HomePageView.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using WpfAnimatedGif;
+using YoutubeDownloader.ViewModels;
 
 namespace YoutubeDownloader.Views
 {
@@ -17,6 +19,16 @@ namespace YoutubeDownloader.Views
             textboxInputUrl.TextChanged += (s, e) => textBlockURLHint.Visibility = textboxInputUrl.Text.Length > 0 ? Visibility.Hidden : Visibility.Visible;
             textboxInputUrl.GotFocus += (s, e) => UrlBarBorder.BorderBrush = new SolidColorBrush(Colors.Blue);
             textboxInputUrl.LostFocus += (s, e) => UrlBarBorder.BorderBrush = new SolidColorBrush(Colors.DarkSlateGray);
+            textboxInputUrl.KeyDown += OnTextboxInputUrlKeyDown;
+        }
+
+        //Search with Enter key
+        private void OnTextboxInputUrlKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            if (this.DataContext == null) return;
+            if(this.DataContext is HomePageViewModel vm)
+                vm.GetVideoData.Execute(null);
         }
 
         private void buttonPaste_Click(object sender, RoutedEventArgs e)
@@ -26,7 +38,7 @@ namespace YoutubeDownloader.Views
 
         private void buttonDownload_Click(object sender, RoutedEventArgs e)
         {
-            //todo
+            //todo: da togliere
         }
 
         private void buttonSearch_Click(object sender, RoutedEventArgs e)
@@ -37,6 +49,7 @@ namespace YoutubeDownloader.Views
             //_mainViewModel.GetVideoMetadata(inputUrl);
         }
 
+        //Loading gif play parameters
         private void imgLoadingGif_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             var img = sender as Image;
