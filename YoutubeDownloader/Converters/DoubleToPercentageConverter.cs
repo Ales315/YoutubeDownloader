24906071e47410b9161490a6917f4e858ed27bd4
Ale <Ale@PC-ALE01>
2024-12-09 15:36:20 +0100

    Added Material Design - layout updates

---
+++ YoutubeDownloader/Converters/DoubleToPercentageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace YoutubeDownloader.Converters
+{
+    class DoubleToPercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                doubleValue *= 100;
+                if (doubleValue == 100)
+                    return $"{doubleValue:0}%";
+                return $"{doubleValue:0.0}%";
+            }
+            return string.Empty;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YoutubeDownloader/UserControls/VideoDownloadCard.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace YoutubeDownloader.UserControls
+{
+    /// <summary>
+    /// Interaction logic for VideoDownloadCard.xaml
+    /// </summary>
+    public partial class VideoDownloadCard : UserControl
+    {
+        public VideoDownloadCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
