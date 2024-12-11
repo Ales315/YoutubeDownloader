622e7bf0838462a5130d140923e3096df2a77652
Alessandro Calabrò <alessandrocala315@gmail.com>
2024-12-11 13:19:32 +0100

    Changed titlebar icons
    Closes #5

--- YoutubeDownloader/MainWindow.xaml.cs
+++ YoutubeDownloader/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using WpfAnimatedGif;
 using YoutubeDownloader.ViewModels;
 
@@ -16,5 +19,17 @@ public partial class MainWindow : Window
         InitializeComponent();
         buttonClose.Click += (s, e) => Close();
         buttonMinimize.Click += (s, e) => WindowState = WindowState.Minimized;
+        buttonClose.MouseEnter += OnButtonCloseMouseEnter;
+        buttonClose.MouseLeave += OnButtonCloseMouseLeave;
+    }
+
+    private void OnButtonCloseMouseLeave(object sender, MouseEventArgs e)
+    {
+        imgClose.Source = new BitmapImage(new Uri(@"/Resources/Images/closeDark24.png", UriKind.Relative));
+    }
+
+    private void OnButtonCloseMouseEnter(object sender, MouseEventArgs e)
+    {
+        imgClose.Source = new BitmapImage(new Uri(@"/Resources/Images/close24.png", UriKind.Relative));
     }
 }
