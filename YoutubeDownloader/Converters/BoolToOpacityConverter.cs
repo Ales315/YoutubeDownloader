d5a810d828d5c9508c1e3e9d45e10bde93650308
Alessandro Calabrò <alessandrocala315@gmail.com>
2024-12-18 00:22:49 +0100

    Button icon opacity based on enabled

--- YoutubeDownloader/Converters/BoolToOpacityConverter.cs
+++ YoutubeDownloader/Converters/BoolToOpacityConverter.cs
@@ -9,8 +9,22 @@ namespace YoutubeDownloader.Converters
         {
             if (value is bool boolValue)
             {
-                var lowOpacity = float.Parse(parameter.ToString()!)/10;
-                return boolValue ? 1.0 : lowOpacity;
+                return boolValue ? 1.0 : 0.3;
+            }
+            return 1;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class InvertedBoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? 0.3 : 1.0;
             }
             return 1;
         }
