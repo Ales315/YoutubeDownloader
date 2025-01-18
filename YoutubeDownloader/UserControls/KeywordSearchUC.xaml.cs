45ff2dafa0c232a1ede377c802cf07ff224d6412
Alessandro Calabrò <alessandrocala315@gmail.com>
2025-01-18 18:30:01 +0100

    Fixed blurry text

--- YoutubeDownloader/UserControls/KeywordSearchUC.xaml.cs
+++ YoutubeDownloader/UserControls/KeywordSearchUC.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using YoutubeDownloader.ViewModels;
+using YoutubeDownloader.ViewModels.Card;
+using YoutubeDownloader.ViewModels.UserControl;
 
 namespace YoutubeDownloader.UserControls
 {
--- YoutubeDownloader/UserControls/VideoUC.xaml.cs
+++ YoutubeDownloader/UserControls/VideoUC.xaml.cs
@@ -4,7 +4,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using WpfAnimatedGif;
 using YoutubeDownloader.Services;
-using YoutubeDownloader.ViewModels;
+using YoutubeDownloader.ViewModels.Card;
+using YoutubeDownloader.ViewModels.UserControl;
 
 namespace YoutubeDownloader.UserControls
 {
