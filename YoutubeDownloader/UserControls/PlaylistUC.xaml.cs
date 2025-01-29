136db31a7f6e467a1f28334b571610ec925b0df0
Alessandro Calabrò <alessandrocala315@gmail.com>
2025-01-29 15:01:46 +0100

    Add playlist user control

---
+++ YoutubeDownloader/UserControls/PlaylistUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace YoutubeDownloader.UserControls
+{
+    /// <summary>
+    /// Interaction logic for KeywordSearchUC.xaml
+    /// </summary>
+    public partial class PlaylistUC : UserControl
+    {
+        public PlaylistUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
